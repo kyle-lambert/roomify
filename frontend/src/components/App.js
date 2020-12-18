f6dc545b312a62dd3e3a2651f6eed42017ae0895
kylelambert001 <kylelambert001@gmail.com>
2020-12-18 23:20:24 +1030

    Initial setup of project with base styles

---
+++ frontend/src/components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.scss";
+
+function App(props) {
+  return (
+    <div>
+      <div className="div a"></div>
+      <div className="div b"></div>
+      <div className="div c"></div>
+      <div className="div d"></div>
+      <div className="div e"></div>
+      <div className="div f"></div>
+      <div className="div g"></div>
+      <div className="div h"></div>
+      <div className="div i"></div>
+      <div className="div j"></div>
+    </div>
+  );
+}
+
+export default App;
